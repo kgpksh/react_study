f8665b74968de029399b65194b4108c47093ca3f
kgpksh <tkdgns543@gmail.com>
2023-11-18 04:14:59 +0900

    함수와 useState 응용으로 간단한 숫자 increment 구현

--- src/learning_1/App.js
+++ src/learning_1/App.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 
 let title = "글제목임";
 let content = "글내용임"
+
+
 function Post() {
+  let [count, countController] = useState(1);
+  function incrementNum() {
+    countController(++count);
+  }
+
   return (
     <div>
       <div className='Post-decorator'>
@@ -10,6 +18,15 @@ function Post() {
       </div>
       <div className='Post-decorator'>
         { content }
+      </div>
+
+      <div>
+        <h1 onClick={ incrementNum }>
+          숫자 증가
+        </h1>
+        <h1>
+          { count }
+        </h1>
       </div>
     </div>
   );
