61a106376a4bf73e295298bc3b20c1f00bc569f1
kgpksh <tkdgns543@gmail.com>
2023-11-18 04:28:43 +0900

    배열형 useState 원소 변경 공부. 기능들 컴포넌트로 분리.

--- src/learning_1/App.js
+++ src/learning_1/App.js
@@ -6,11 +6,6 @@ let content = "글내용임"
 
 
 function Post() {
-  let [count, countController] = useState(1);
-  function incrementNum() {
-    countController(++count);
-  }
-
   return (
     <div>
       <div className='Post-decorator'>
@@ -19,22 +14,55 @@ function Post() {
       <div className='Post-decorator'>
         { content }
       </div>
-
-      <div>
-        <h1 onClick={ incrementNum }>
-          숫자 증가
-        </h1>
-        <h1>
-          { count }
-        </h1>
-      </div>
     </div>
   );
+}
+
+function ShowNum() {
+  let [count, countController] = useState(1);
+
+  function incrementNum() {
+    countController(++count);
+  }
+
+  return (
+    <div>
+      <h1 onClick={ incrementNum }>
+        숫자 증가
+      </h1>
+      <h1>
+        { count }
+      </h1>
+    </div>
+  )
+}
+
+function ShowText() {
+  const [element, elementController] = useState(['aaa', 'bbb', 'ccc']);
+
+  function arrayChanger() {
+    let copy =[...element];
+    copy[0] = 'ddddd';
+    elementController(copy);
+  }
+
+  return (
+    <div>
+      <h1 onClick={ arrayChanger }>
+        문자 변경
+      </h1>
+      <h1>
+        { element[0] }
+      </h1>
+    </div>
+  )
 }
 function App() {
   return (
     <div>
       <Post></Post>
+      <ShowNum></ShowNum>
+      <ShowText></ShowText>
     </div>
   );
 }
