128987ce14ceb5471d4893806f87de4306920139
kgpksh <tkdgns543@gmail.com>
2023-11-19 08:09:38 +0900

    라우터 도입

--- src/learning_2/App.js
+++ src/learning_2/App.js
@@ -1,11 +1,22 @@
 import DayList from './DayList'
 import Day from './Day'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 function App() {
     return (
-        <div>
-            <DayList></DayList>
-            <Day></Day>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path='/'>
+                        <DayList></DayList>
+                    </Route>
+                    <Route>
+                        <Day></Day>
+                    </Route>
+                    
+                </Switch>
+            </div>
+        </BrowserRouter>
+        
     );
   }
   
